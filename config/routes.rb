eab2e8a7b4f6a30dd3a81d5a3af78a6ca15a1c6a
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-24 17:57:46 +0100

    Route fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'restaurants/:id', to: 'restaurants#show'
 
   get 'restaurants/:restaurant_id/products', to: 'restaurants#products'
-  get 'restaurants/:restaurant_id/products/type/:pet', to: 'restaurants#products_for_pet'
+  get 'restaurants/:restaurant_id/products/pet/:pet', to: 'restaurants#products_for_pet'
 
   get 'products/', to: 'products#index'
   get 'products/pet/:pet', to: 'products#products_for_pet'
