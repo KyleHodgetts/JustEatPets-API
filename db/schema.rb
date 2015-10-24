287ccccf4ea59278954c16e2b15fae9b5ac9a811
Fares Alaboud <fares.alaboud@gmail.com>
2015-10-24 17:05:54 +0100

    added photo url

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151024152011) do
+ActiveRecord::Schema.define(version: 20151024160518) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at",    null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20151024152011) do
     t.float    "price"
     t.string   "pet"
     t.integer  "restaurant_id"
+    t.string   "photo"
   end
 
   create_table "restaurants", force: :cascade do |t|
