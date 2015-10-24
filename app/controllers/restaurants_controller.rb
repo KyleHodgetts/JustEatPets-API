2a7a5b61557fce1c2b3d75379eff12314426e543
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-24 17:46:47 +0100

    Restaurant data retrieval complete

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,33 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    render json: @restaurants
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+    render json: @restaurant
+  end
+
+  def products
+    render json: restaurant_products
+  end
+
+  def products_for_pet
+    @products = restaurant_products
+    @products.select do |product|
+      product.pet = params[:pet]
+    end
+    render json: @products
+  end
+
+  private
+
+  def restaurant_products
+    @products = []
+    if restaurant = Restaurant.find(params[:restaurant_id])
+      @products = restaurant.products
+    end
+    @products
+  end
+end
