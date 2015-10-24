a6ce09d6246cb5a253b45b0cff3bbbe4989dc8e4
Fares Alaboud <fares.alaboud@gmail.com>
2015-10-24 18:50:59 +0100

    new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,23 +6,43 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-restaurants = [{name: "Jana's Pet Store", postcode: "B15 2TT"}, 
-	{name: "Josh's Pet Store", postcode: "B15 2TQ"}]
+# OLD SEED
 
-products = [{name: "Cat Pouch", pet: "cat", price: 24.2}, 
-	{name: "Dog Bag", pet: "dog", price: 34.2}, 
-	{name: "Llama Food", pet: "llama", price: 12.2},
-	{name: "Lettuce", pet: "turtle", price: 0.90}]
+# restaurants = [{name: "Jana's Pet Store", postcode: "B15 2TT"}, 
+# 	{name: "Josh's Pet Store", postcode: "B15 2TQ"}]
 
-products.each do |p|
-	Product.create(name: p[:name], pet: p[:pet], price: p[:price])
+# products = [{name: "Cat Pouch", pet: "cat", price: 24.2}, 
+# 	{name: "Dog Bag", pet: "dog", price: 34.2}, 
+# 	{name: "Llama Food", pet: "llama", price: 12.2},
+# 	{name: "Lettuce", pet: "turtle", price: 0.90}]
+
+# products.each do |p|
+# 	Product.create(name: p[:name], pet: p[:pet], price: p[:price])
+# end
+
+# restaurants.each do |r|
+# 	Restaurant.create(name: r[:name], postcode: r[:postcode])
+# end
+
+# Restaurant.find_by_name("Jana's Pet Store").products << Product.find_by_name("Cat Pouch")
+# Restaurant.find_by_name("Jana's Pet Store").products << Product.find_by_name("Dog Bag")
+# Restaurant.find_by_name("Josh's Pet Store").products << Product.find_by_name("Llama Food")
+# Restaurant.find_by_name("Josh's Pet Store").products << Product.find_by_name("Lettuce")
+
+require 'csv'
+
+file = File.read("db/import/pet-food-from-sainsburys.csv"); nil
+
+csv = CSV.parse(file, :headers => true)
+
+csv.each do |row|
+
+Product.create!(row.to_hash)
+
 end
 
-restaurants.each do |r|
-	Restaurant.create(name: r[:name], postcode: r[:postcode])
-end
+Product.all.map{ |product| product.name = product.name.split('Only').first.strip; product.save if product.name_changed? }
 
-Restaurant.find_by_name("Jana's Pet Store").products << Product.find_by_name("Cat Pouch")
-Restaurant.find_by_name("Jana's Pet Store").products << Product.find_by_name("Dog Bag")
-Restaurant.find_by_name("Josh's Pet Store").products << Product.find_by_name("Llama Food")
-Restaurant.find_by_name("Josh's Pet Store").products << Product.find_by_name("Lettuce")
+Product.all.map{ |product| product.name = product.name.split('£').first.strip; product.save if product.name_changed? }
+
+Product.all.map(&:name)
