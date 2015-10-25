62332cd681cafa876b194358bbf572aa99527e0a
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-25 08:37:42 +0000

    Clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,15 @@ file = File.read("db/import/pet-food-from-sainsburys.csv"); nil
 
 csv = CSV.parse(file, :headers => true)
 
+restaurants = [{name: "Jolly Joe's", postcode: "B15 2TT"},
+  {name: "Josh's Fridge", postcode: "B15 2TQ"},
+  {name: "Yummy Jana's", postcode: "W14 8QW"},
+  {name: "Mamma Maria's", postcode: "W14 8HW"} ]
+
+restaurants.each do |r|
+  Restaurant.create(name: r[:name], postcode: r[:postcode])
+end
+
 csv.each do |row|
 
   Product.create!(row.to_hash)
@@ -16,11 +25,4 @@ Product.all.map{ |product| product.name = product.name.split('£').first.strip; 
 
 Product.all.map(&:name)
 
-restaurants = [{name: "Jolly Joe's", postcode: "B15 2TT"},
-  {name: "Josh's Fridge", postcode: "B15 2TQ"},
-  {name: "Yummy Jana's", postcode: "W14 8QW"},
-  {name: "Mamma Maria's", postcode: "W14 8HW"} ]
 
-restaurants.each do |r|
-	Restaurant.create(name: r[:name], postcode: r[:postcode])
-end
