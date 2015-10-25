15a4f0889eaba0092aa624887d0845d22e53a39b
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-25 08:24:14 +0000

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,8 +31,6 @@ end
 
 require 'csv'
 
-Product.delete_all
-
 file = File.read("db/import/pet-food-from-sainsburys.csv"); nil
 
 csv = CSV.parse(file, :headers => true)
@@ -48,3 +46,12 @@ Product.all.map{ |product| product.name = product.name.split('Only').first.strip
 Product.all.map{ |product| product.name = product.name.split('£').first.strip; product.save if product.name_changed? }
 
 Product.all.map(&:name)
+
+restaurants = [{name: "Jolly Joe's", postcode: "B15 2TT"},
+  {name: "Josh's Fridge", postcode: "B15 2TQ"},
+  {name: "Yummy Jana's", postcode: "W14 8QW"},
+  {name: "Mamma Maria's", postcode: "W14 8HW"} ]
+
+restaurants.each do |r|
+	Restaurant.create(name: r[:name], postcode: r[:postcode])
+end
