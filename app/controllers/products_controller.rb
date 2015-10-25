fdb859b5c7658109883d15a072c886411ffa9798
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-25 10:34:09 +0000

    append zero

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,15 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @products.each do |p|
+      append_zero(p.price)
+    end
     render json: @products
   end
 
   def show
     @product = Product.find(params[:id])
+    @product.price = append_zero(@product.price)
     render json: @product
   end
 
@@ -14,9 +18,16 @@ class ProductsController < ApplicationController
     products_to_return = []
     @products.each do |product|
       if product.pet == params[:pet]
+        product.price = append_zero(product.price)
         products_to_return << product
       end
     end
     render json: products_to_return
+  end
+
+  private
+
+  def append_zero(x)
+    format('%.2f', x)
   end
 end
