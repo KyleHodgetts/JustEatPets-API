f975cd389799097ff94497b11fce7c6bd92df137
Kyle Hodgetts <kyle.hodgetts@kcl.ac.uk>
2015-10-25 08:20:02 +0000

    Fixed silly boolean checking

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,12 +10,13 @@ class ProductsController < ApplicationController
   end
 
   def products_for_pet
-    @products =  Product.all
-    @products.select do |product|
-      product.pet = params[:pet]
+    @products = Product.all
+    products_to_return = []
+    @products.each do |product|
+      if product.pet == params[:pet]
+        products_to_return << product
+      end
     end
-    render json: @products
+    render json: products_to_return
   end
-
-
 end
